3f7ab34fd5fbc94405eaa8e5439d2f1e89d35968
Oluwatobi Ogunbayo <tobyogunbayo@gmail.com>
2022-12-06 19:53:30 +0100

    fix: fix typo

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "docker" {
   registry_auth {
     address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_caller_identity.current.region}.amazonaws.com"
-    username = data.aws_ecr_authorization_token.token.user_name
-    password = data.aws_ecr_authorization_token.token.password
+    username = data.aws_ecr_authorization_token.current.user_name
+    password = data.aws_ecr_authorization_token.current.password
   }
 }
